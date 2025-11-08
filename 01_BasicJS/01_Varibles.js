62450bc2a326355a1fe1843197c4fa4a67445579
192anchal <anchalprajapati241@gmail.com>
2025-11-08 23:04:20 +0530

    Updated JS file and removed old folder

--- 01_BasicJS/01_Varibles.js
+++ 01_BasicJS/01_Varibles.js
@@ -2,8 +2,13 @@ const accountID=144553
 let email="vashnavi@gmail.com"
 var accountPassword="12345"
 accountCity="Jaipur"
+accountPassword="2344"
+email="asnadk@fjdh"
+accountCity="dhsfihf"
 
 
 
-accountID=2
+//accountID=2  const does not change const is a unique variableno
 console.log(accountID);
+console.table([accountID,accountPassword,accountCity,email]);
+
