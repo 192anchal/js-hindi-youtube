df72feccd1dc5b46673af1a9329cc22eb9916a3d
192anchal <anchalprajapati241@gmail.com>
2025-11-09 00:12:13 +0530

    comperisionOperation

---
+++ 01_BasicJS/conversionOperation.js
@@ -0,0 +1,31 @@
+let str1=2
+let str2=3
+let str3=str1+str2
+console.log(str3);
+/* console.log("1"+2);//12
+console.log("2"+1);//12
+console.log("1"+2+2);//122
+console.log(2+2+"3");//43
+console.log((3+3)*5%3); */
+/* console.log(true);
+console.log(+true);
+console.log(+"");
+ */
+
+let num1,num2,num3
+
+num1=num2=num3=2+3
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+
+
+//js mdn for prefix post fix
+
+
+
+
+
+
+
+
