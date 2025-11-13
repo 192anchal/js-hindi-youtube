f98f5d713a81313e1ef4501bbc54f603f0ac3573
192anchal <anchalprajapati241@gmail.com>
2025-11-13 17:45:06 +0530

    object

---
+++ 01_BasicJS/02_Objects.js
@@ -0,0 +1,13 @@
+//object literet declaration of object,, const jsobj={}
+//singleton object create by constructor=== object.create
+
+const jsobj={
+    name:"Anchal",
+    age:23,
+    location:"jaipur",
+    
+}
+console.log(jsobj.name);
+console.log(jsobj["name"]);
+
+
